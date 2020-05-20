3a7ead95a74517ad0d33da89ee7a8f43b35be2de
Alan <alanvega002@gmail.com>
2020-05-20 01:14:18 -0800

    ipfs node

--- newpieceplease.js
+++ newpieceplease.js
@@ -1,23 +1,21 @@
-class NewPiecePlease {
-  constructor(Ipfs, OrbitDB) {
-    this.OrbitDB = OrbitDB;
+const IPFS = require("ipfs");
+const OrbitDB = require("orbit-db");
 
-    this.node = new Ipfs({
-      preload: { enabled: false },
-      repo: "./ipfs",
-      EXPERIMENTAL: { pubsub: true },
-      config: {
-        Bootstrap: [],
-      },
-    });
+async function createNode() {
+  let node = await IPFS.create({
+    repo: (() => `repo-${Math.random()}`)(),
+    Addresses: {
+      Swarm: ["/ip4/0.0.0.0/tcp/4001"],
+      API: "/ip4/127.0.0.1/tcp/5001",
+      Gateway: "/ip4/127.0.0.1/tcp/8080",
+    },
+  });
+  try {
+    await node.start();
+    console.log("Node started!");
+  } catch (error) {
+    console.error("Node failed to start!", error);
   }
 }
 
-try {
-  const Ipfs = require("ipfs");
-  const OrbitDB = require("orbit-db");
-
-  module.exports = exports = new NewPiecePlease(Ipfs, OrbitDB);
-} catch (e) {
-  window.NPP = new NewPiecePlease(window.Ipfs, window.OrbitDB);
-}
+console.log(createNode());
